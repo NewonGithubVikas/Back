d612f17cb45196ebed90f1cd405b755eb91f790b
Vikas <166400180+NewonGithubVikas@users.noreply.github.com>
2025-03-10 15:28:07 +0530

    Update apisModel.js

--- model/apisModel.js
+++ model/apisModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-const uri="mongodb://localhost:27017/Red";
-// const uri = process.env.DB_URL;
+// const uri="mongodb://localhost:27017/Red";
+const uri = process.env.DB_URL;
 const createDefaultAdmin = require("../utils/createDefaultAdmin"); 
 const staticContentSchema = new mongoose.Schema({
     title: {
